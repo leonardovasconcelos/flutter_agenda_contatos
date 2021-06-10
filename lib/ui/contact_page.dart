07fb3d6067d848779f130ca583f7a6f050e5c552
leonardovasconcelos <leonardovasconcelos@id.uff.br>
2021-06-10 10:20:04 -0300

    Inserindo a função de ordenação dos contatos

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_agenda_contatos/helpers/contact_helper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ContactPage extends StatefulWidget {
   final Contact contact;
@@ -74,6 +75,14 @@ class _ContactPageState extends State<ContactPage> {
                               ? FileImage(File(_editedContact.img))
                               : AssetImage("images/person.png"))),
                 ),
+                onTap: (){
+                  ImagePicker.pickImage(source: ImageSource.camera).then((file) {
+                    if(file==null) return;
+                    setState(() {
+                      _editedContact.img = file.path;
+                    });
+                  });
+                },
               ),
               TextField(
                 controller: _nameController,
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter_agenda_contatos/ui/contact_page.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+enum OrderOptions{orderaz, orderza}
 
 class HomePage extends StatefulWidget {
   @override
@@ -28,6 +31,21 @@ class _HomePageState extends State<HomePage> {
         title: Text("Contatos"),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<OrderOptions>(
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de A-Z"),
+                value: OrderOptions.orderaz,
+              ),
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de Z-A"),
+                value: OrderOptions.orderza,
+              )
+            ],
+            onSelected: _orderList,
+          )
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -112,7 +130,8 @@ class _HomePageState extends State<HomePage> {
                             style: TextStyle(color: Colors.red, fontSize: 20.0),
                           ),
                           onPressed: (){
-
+                            launch("tel:${contacts[index].phone}");
+                            Navigator.pop(context);
                           },
                         ),
                       ),
@@ -171,6 +190,24 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         contacts = list;
       });
+    });
+  }
+
+  void _orderList(OrderOptions result){
+    switch(result){
+      case OrderOptions.orderaz:
+        contacts.sort((a,b){
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        contacts.sort((a,b){
+          return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });
+        break;
+    }
+    setState(() {
+
     });
   }
 }
